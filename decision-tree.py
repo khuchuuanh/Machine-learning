84d8d3359ff9318c8a3274e53e9b2c7498cb3f79
KhucHuuAnh <khuchuuanh@gmail.com>
2023-05-09 13:00:47 +0700

    this code about decision tree

--- decision-tree.py
+++ decision-tree.py
@@ -5,6 +5,34 @@ from sklearn.preprocessing import LabelEncoder
 from sklearn import tree
 import graphviz 
 
+
+class TreeNode(object):
+    def __init__(self, ids = None,  children = [], entropy = 0, depth = 0):
+        self.ids = ids
+        self.children = children
+        self.entropy = entropy
+        self.depth = depth
+        self.split_attribute = None
+        self.order = None
+        self.label  = None
+
+    
+    def set_properties(self, split_attribute, order):
+        self.split_attribute = split_attribute
+        self.order = order
+
+
+    def set_lable(self, label):
+        self.label = label
+
+
+def entropy(freq):
+    # remove prob
+    freq_0 = freq[np.array(freq).nonzero()[0]]
+    prob_0 = freq_0/float(freq_0.sum())
+    return -np.sum(prob_0*np.log(prob_0))
+
+
 df = pd.read_csv('D:/machine learning/PlayTennis.csv')
 #print(df.dtypes)
 
