8b028c9094b7dc47479d933ac6866aceabad702f
KhucHuuAnh <khuchuuanh@gmail.com>
2023-05-09 20:33:33 +0700

    this code about decision tree

--- decision-tree.py
+++ decision-tree.py
@@ -33,6 +33,47 @@ def entropy(freq):
     return -np.sum(prob_0*np.log(prob_0))
 
 
+class DecisionTreeID3(object):
+    def __init__(self, max_depth = 10, min_samples_split = 2, min_gain = 1e-4):
+        self.root = None
+        self.max_depth = max_depth
+        self.min_samples_split = min_samples_split
+        self.Ntrain = 0
+        self.min_gain = min_gain
+
+    def fit(self, data, target):
+        self.Ntrain = data.count()[0]
+        self.data =data
+        self.target = target
+        self.labels = target.unique()
+
+        ids = range(self.Ntrain)
+        self.root = TreeNode(ids = ids, entropy = self._entropy(ids), depth= 0)
+        queue = [self.root]
+
+        while queue:
+            node  = queue.pop()
+            if node.depth < self.max_depth or node.entropy < self.min_gain:
+                node.children = self._split(node)
+                if not node.children:
+                    self._set_label(node)
+                queue += node.children
+            else:
+                self._set_label(node)
+
+    
+    def _entropy(self, ids):
+        if len(ids) ==0:
+            return 0
+        
+        ids = [i+1  for i in ids]
+        freq = np.array(self.target[ids].value_counts())
+        return entropy(freq)
+
+    
+
+
+
 df = pd.read_csv('D:/machine learning/PlayTennis.csv')
 #print(df.dtypes)
 
