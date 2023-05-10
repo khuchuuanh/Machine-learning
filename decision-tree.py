faaeccaf48d14196ac6d8e070c7b7647652ea847
KhucHuuAnh <khuchuuanh@gmail.com>
2023-05-10 10:30:23 +0700

    this code about decision-tree

--- decision-tree.py
+++ decision-tree.py
@@ -69,8 +69,59 @@ class DecisionTreeID3(object):
         ids = [i+1  for i in ids]
         freq = np.array(self.target[ids].value_counts())
         return entropy(freq)
-
     
+    def _set_label(self, node):
+        target_ids = [i + 1 for i in  node.ids]
+        node.set_label(self.target[target_ids].mode()[0])
+    
+    def _split(self, node):
+        ids = node.ids
+        best_gain = 0
+        best_splits = 0
+        best_attribute = None
+        order = None
+        sub_data = self.data.iloc[ids, :]
+        for i , att  in enumerate(self.attributes):
+            values = self.data.iloc[ids,i].unique().tolist()
+            if len(values) == 1:
+                continue
+            splits = []
+            HxS = 0
+            for split in splits:
+                HxS += len(split)*self._entropy(split)/len(ids)
+            gain  = node.entropy -HxS
+            if gain < self.min_gain:
+                continue
+            if gain > best_gain:
+                best_gain = gain
+                best_splits = split
+                best_attribute = att
+                order =  values
+        node.set_properties(best_attribute, order)
+        child_nodes = [TreeNode(ids = split, entropy = self._entropy(split), depth = node.depth + 1) for split in best_splits ]
+        return child_nodes
+    
+
+    def predict(self, new_data):
+        npoints =   new_data.count()[0]
+        labels = [None]*npoints
+        for n in range(npoints):
+            x  = new_data.iloc[n, :]
+            node = self.root
+            while node.children:
+                node = node.children[node.order.index(x[node.split_attribute])]
+            labels[n] = node.label
+        return labels
+        
+'''
+if __name__ == '__main__':
+    df = pd.read_csv('D:/machine learning/PlayTennis.csv')
+    X = df.drop(['play'], axis= 1)
+    y = df['play']
+
+    tree = DecisionTreeID3(max_depth= 3, min_samples_split=2)
+    tree.fit(X,y)
+    print(tree.predict(X))'''
 
 
 
@@ -93,12 +144,8 @@ clf = tree.DecisionTreeClassifier(criterion = 'entropy')
 clf = clf.fit(X, y)
 tree.plot_tree(clf)
 
-
-
 dot_data = tree.export_graphviz(clf, out_file=None) 
 graph = graphviz.Source(dot_data) 
-
-
 X_pred = clf.predict([['2','1','0','0']])
 
 print(X_pred)
